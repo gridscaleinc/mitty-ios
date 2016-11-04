267ed108342f2b55a9f4823b31d0fcf2d32bd010
domanthan <domanthan@gmail.com>
2016-11-05 00:17:40 +0900

    余計なクラスを削除

--- mitty/EventDetailViewController.swift
+++ mitty/EventDetailViewController.swift
@@ -148,38 +148,3 @@ class EventDetailViewController : UIViewController {
 
 
 }
-
-//
-//  ModalViewController.swift
-//  SampleModalBackground
-//
-//  Created by fuhi1983 on 2015/01/25.
-//  Copyright (c) 2015年 sample. All rights reserved.
-//
-import UIKit
-
-class ModalViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        view.backgroundColor = UIColor(red: 0.8, green: 0.1, blue: 0.2, alpha: 0.4)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    /*
-     // MARK: - Navigation
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-}
