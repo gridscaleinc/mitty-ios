75845d7f836d61a2044d677f0a7788924151490d
Dongri Jin <dongrify@gmail.com>
2017-03-06 13:52:58 +0900

    Added Alamofire

--- mitty/Service.swift
+++ mitty/Service.swift
@@ -6,6 +6,32 @@
 //  Copyright © 2016年 GridScale Inc. All rights reserved.
 //
 
+
 import Foundation
+import Alamofire
 
 // Serverとの通信を使う
+func SignUp(username: String, password: String) {
+    let params = [
+        "username": username,
+        "password": password
+    ]
+    Alamofire.request("http://dev.mitty.co/api/signup", method: .post, parameters: params, encoding: URLEncoding.default).responseJSON { response in
+        debugPrint(response)
+        if let json = response.result.value {
+            print("JSON: \(json)")
+        }
+    }
+}
+
+func SignIn(username: String, password: String) {
+    let params = [
+        "username": username,
+        "password": password
+    ]
+    Alamofire.request("http://dev.mitty.co/api/signin", method: .post, parameters: params, encoding: URLEncoding.default).responseJSON { response in       debugPrint(response)
+        if let json = response.result.value {
+            print("JSON: \(json)")
+        }
+    }
+}
