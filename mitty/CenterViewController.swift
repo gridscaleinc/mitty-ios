7e43b53f625e15c2af802618ec295ee98ed93735
domanthan <domanthan@gmail.com>
2017-03-31 00:14:25 +0900

    地図の機能をつけ始める。

--- mitty/CenterViewController.swift
+++ mitty/CenterViewController.swift
@@ -11,12 +11,19 @@ import Foundation
 import UIKit
 import MapKit
 
+import CoreLocation
+
 //
 // 個人情報を管理するView
 //
 @objc(CenterViewController)
-class CenterViewController: UIViewController {
+class CenterViewController: UIViewController, CLLocationManagerDelegate,MKMapViewDelegate {
    
+    //LocationManagerの生成（viewDidLoadの外に指定してあげることで、デリゲートメソッドの中でもmyLocationManagerを使用できる）
+    let myLocationManager = CLLocationManager()
+    
+    let myMapView = MKMapView()
+    
     let form = MQForm.newAutoLayout()
     
     // Autolayout済みフラグ
@@ -45,10 +52,18 @@ class CenterViewController: UIViewController {
         let swiftColor = UIColor(red: 0.3, green: 0.5, blue: 0.6, alpha: 1)
         self.view.backgroundColor = swiftColor
         
-        let myMapView = MKMapView()
+
         myMapView.frame = self.view.frame
         self.view.addSubview(myMapView)
         
+        //長押しを探知する機能を追加
+        let longTapGesture = UILongPressGestureRecognizer()
+        longTapGesture.addTarget(self, action: #selector(longPressed))
+        myMapView.addGestureRecognizer(longTapGesture)
+        myMapView.showsUserLocation = true
+
+        //ここからが現在地取得の処理
+        myLocationManager.delegate = self
         
         let rect = CGRect(x:0, y:0, width:40, height: 40/1.414)
         
@@ -125,6 +140,15 @@ class CenterViewController: UIViewController {
         // 各サブビューのupdateViewConstraintsを再帰的に呼び出す。
         view.setNeedsUpdateConstraints()
         
+        let status = CLLocationManager.authorizationStatus()
+        if status == CLAuthorizationStatus.notDetermined {
+            // まだ承認が得られていない場合は、認証ダイアログを表示
+            myLocationManager.requestAlwaysAuthorization()
+        }
+        // 位置情報の更新を開始
+        myLocationManager.startUpdatingLocation()
+        
+        
     }
     
     //
@@ -158,5 +182,77 @@ class CenterViewController: UIViewController {
         self.navigationItem.title = "..."
         self.tabBarController?.tabBar.isHidden = true
         self.navigationController?.pushViewController(eidtorView, animated: true)
+    }
+    
+    //長押しした時にピンを置く処理
+    func longPressed(sender: UILongPressGestureRecognizer) {
+        
+        //この処理を書くことにより、指を離したときだけ反応するようにする（何回も呼び出されないようになる。最後の話したタイミングで呼ばれる）
+        if sender.state != UIGestureRecognizerState.began {
+            return
+        }
+        
+        //senderから長押しした地図上の座標を取得
+        let  tappedLocation = sender.location(in: myMapView)
+        let tappedPoint = myMapView.convert(tappedLocation, toCoordinateFrom: myMapView)
+        
+        //ピンの生成
+        let pin = MKPointAnnotation()
+        //ピンを置く場所を指定
+        pin.coordinate = tappedPoint
+        //ピンのタイトルを設定
+        pin.title = "中野区"
+        //ピンのサブタイトルの設定
+        pin.subtitle = "区役所付近"
+        //ピンをMapViewの上に置く
+        self.myMapView.addAnnotation(pin)
+    }
+    
+    var isStarting = true
+    var currentLocationPin = MKPointAnnotation()
+    // GPSから値を取得した際に呼び出されるメソッド
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // 配列から現在座標を取得（配列locationsの中から最新のものを取得する）
+        let myLocation = locations.last! as CLLocation
+        //Pinに表示するためにはCLLocationCoordinate2Dに変換してあげる必要がある
+        let currentLocation = myLocation.coordinate
+        //ピンの生成と配置
+        currentLocationPin.coordinate = currentLocation
+
+        if isStarting {
+            currentLocationPin.title = "現在地"
+            self.myMapView.addAnnotation(currentLocationPin)
+            //アプリ起動時の表示領域の設定
+            //delta数字を大きくすると表示領域も広がる。数字を小さくするとより詳細な地図が得られる。
+            let mySpan = MKCoordinateSpan(latitudeDelta: 0.0005, longitudeDelta: 0.0005)
+            let myRegion = MKCoordinateRegionMake(currentLocation, mySpan)
+            myMapView.region = myRegion
+            isStarting = false
+        }
+    }
+    
+    //GPSの取得に失敗したときの処理
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    //認証状態を確認するだけなので、ここの処理はなくてもOK
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // 認証のステータスをログで表示.
+        var statusStr = ""
+        switch (status) {
+        case .notDetermined:
+            statusStr = "NotDetermined"
+        case .restricted:
+            statusStr = "Restricted"
+        case .denied:
+            statusStr = "Denied"
+        case .authorizedAlways:
+            statusStr = "AuthorizedAlways"
+        case .authorizedWhenInUse:
+            statusStr = "AuthorizedWhenInUse"
+        }
+        print(" CLAuthorizationStatus: \(statusStr)")
+        
     }
 }
