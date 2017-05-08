f98dff76e104da77bdcab3ef2b052545093d6386
domanthan <domanthan@gmail.com>
2017-05-09 08:22:16 +0900

    ちょっと

--- mitty/IslandViewController.swift
+++ mitty/IslandViewController.swift
@@ -5,14 +5,13 @@ import PureLayout
 @objc(IsLandViewController)
 class IslandViewController: UIViewController {
     
+    var meetingSegments : UISegmentedControl = UISegmentedControl(items: ["Public", "Private"])
     
     // MARK: - Properties
     var islands: [Island]
     
     // MARK: - View Elements
     let topImage: UIImageView
-    let buttonPublic: UIButton
-    let buttonPrivate: UIButton
     
     let collectionView: UICollectionView
     
@@ -21,17 +20,6 @@ class IslandViewController: UIViewController {
         islands = [Island]()
         self.topImage = UIImageView.newAutoLayout()
         self.topImage.image = UIImage(named: "penginland")
-        
-        self.buttonPublic = UIButton.newAutoLayout()
-        self.buttonPublic.setTitle("public", for: UIControlState.normal)
-        self.buttonPublic.setTitleColor(UIColor.black, for: .normal)
-        self.buttonPublic.backgroundColor = UIColor(red: 0.3, green: 0.5, blue: 0.6, alpha: 0.9)
-        
-        self.buttonPrivate = UIButton.newAutoLayout()
-        self.buttonPrivate.setTitle("Private", for: UIControlState.normal)
-        self.buttonPrivate.backgroundColor = UIColor.gray
-        
-        self.buttonPrivate.setTitleColor(UIColor.darkGray, for: .normal)
         
         self.collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
         self.collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -63,9 +51,8 @@ class IslandViewController: UIViewController {
     private func configureNavigationBar() {}
     
     private func addSubviews() {
+        view.addSubview(meetingSegments)
         view.addSubview(topImage)
-        view.addSubview(buttonPublic)
-        view.addSubview(buttonPrivate)
         view.addSubview(collectionView)
     }
     
@@ -82,17 +69,14 @@ class IslandViewController: UIViewController {
         topImage.autoPinEdge(toSuperviewEdge: .left)
         topImage.autoPinEdge(toSuperviewEdge: .right)
         
-        buttonPublic.autoPinEdge(.top, to: .bottom, of: topImage )
-        buttonPublic.autoPinEdge(.left, to: .left, of: topImage )
-        buttonPublic.autoSetDimension(.height, toSize: 25)
-        buttonPublic.autoSetDimension(.width, toSize: UIScreen.main.bounds.size.width / 2)
         
-        buttonPrivate.autoPinEdge(.top, to: .top, of: buttonPublic )
-        buttonPrivate.autoPinEdge(.left, to: .right, of: buttonPublic )
-        buttonPrivate.autoPinEdge(.right, to: .right, of: topImage )
-        buttonPrivate.autoPinEdge(.bottom, to: .bottom, of: buttonPublic )
+        meetingSegments.selectedSegmentIndex = 0
+        meetingSegments.translatesAutoresizingMaskIntoConstraints = false
+        meetingSegments.autoPinEdge(.top, to: .bottom, of: topImage, withOffset: 10)
+        meetingSegments.autoPinEdge(toSuperviewEdge: .left, withInset: 0)
+        meetingSegments.autoPinEdge(toSuperviewEdge: .right, withInset: 0)
         
-        collectionView.autoPinEdge(.top, to: .bottom, of: buttonPublic)
+        collectionView.autoPinEdge(.top, to: .bottom, of: meetingSegments, withOffset: 3)
         collectionView.autoPinEdge(toSuperviewEdge: .left)
         collectionView.autoPinEdge(toSuperviewEdge: .right)
         collectionView.autoPinEdge(toSuperviewEdge: .bottom)
