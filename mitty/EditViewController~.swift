648faa24bec1126baf1b6dd1abac3464c34f2e82
domanthan <domanthan@gmail.com>
2017-10-18 20:53:59 +0900

    meetingIDを追加。

---
+++ mitty/EditViewController~.swift
@@ -0,0 +1,152 @@
+//
+//  EditViewController.swift
+//  mitty
+//
+//  Created by gridscale on 2017/07/20.
+//  Copyright © 2017年 GridScale Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class EditViewController: MittyViewController {
+
+    var form = MQForm.newAutoLayout()
+
+
+    let activityTitle = MQForm.text(name: "title", placeHolder: "タイトルを入力してください")
+
+    let memo = MQForm.textView(name: "memo")
+
+    var updateButton = MQForm.button(name: "update", title: "保存")
+
+    var activityInfo: ActivityInfo
+
+    init(_ activity: ActivityInfo) {
+        activityInfo = activity
+        activityTitle.textField.text = activityInfo.title
+        memo.textView.text = activityInfo.memo
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+
+        super.loadView()
+
+        self.form.translatesAutoresizingMaskIntoConstraints = false
+
+        self.view.addSubview(form)
+
+        form +++ activityTitle.layout {
+            t in
+            t.upper(withInset: 10).leftMost(withInset: 10).rightMost(withInset: 10).height(50)
+
+        }
+
+        let header = MQForm.label(name: "header", title: "<メモ>").layout {
+            t in
+            t.label.textColor = .black
+            t.putUnder(of: self.activityTitle).leftMost(withInset: 10).rightMost(withInset: 10).height(30).upMargin(20)
+        }
+        
+        form +++ header
+        
+        form +++ memo.layout {
+            m in
+            m.putUnder(of: header, withOffset: 2)
+                .leftMost(withInset: 10).rightMost(withInset: 10).height(100)
+        }
+
+        form +++ updateButton.layout { [weak self] b in
+            b.button.backgroundColor = .white
+            b.button.setTitleColor(MittyColor.orange, for: .normal)
+            b.holizontalCenter().width(130).height(45).putUnder(of: (self?.memo)!, withOffset: 10)
+        }
+
+        let deleteButton = MQForm.button(name: "delete", title: "活動計画を削除")
+        form +++ deleteButton
+        deleteButton.layout() {
+            c in
+            c.view.backgroundColor = UIColor.red
+            c.view.layer.cornerRadius = 8
+            c.button.setTitleColor(.white, for: .normal)
+            c.height(28).fillHolizon(60).putUnder(of: self.updateButton, withOffset: 40)
+        }
+        
+        let warningMsg = MQForm.label(name: "alert", title: "◆注意：\r\n 活動計画を削除すると、付随する全ての予定も削除される。")
+        form +++ warningMsg.layout {
+            w in
+            w.label.backgroundColor = UIColor.lightGray
+            w.label.textColor = MittyColor.baseYellow
+            w.label.layer.cornerRadius = 3
+            w.label.layer.masksToBounds = true
+            w.label.numberOfLines = 0
+            w.leftMost(withInset: 20).rightMost(withInset: 20).putUnder(of: deleteButton, withOffset: 30)
+        }
+        
+        deleteButton.bindEvent(.touchUpInside) {
+            b in
+            ActivityService.instance.removeActivity(id: Int64(self.activityInfo.id)!) {
+                self.navigationController?.popToRootViewController(animated: true)
+            }
+        }
+        
+        view.setNeedsUpdateConstraints() // bootstrap Auto Layout
+
+    }
+
+
+    override func updateViewConstraints() {
+        form.autoPin(toTopLayoutGuideOf: self, withInset: 0)
+        form.autoPinEdge(toSuperviewEdge: .left)
+        form.autoPinEdge(toSuperviewEdge: .right)
+        form.autoPinEdge(toSuperviewEdge: .bottom)
+
+        form.configLayout()
+        super.updateViewConstraints()
+
+    }
+
+    override func viewDidLoad() {
+
+        super.autoCloseKeyboard()
+
+        self.navigationItem.title = "活動編集"
+
+        self.view.backgroundColor = UIColor.white
+
+        updateButton.bindEvent(.touchUpInside) {
+            [weak self] b in
+
+            self?.updateActivity()
+
+        }
+
+        LoadingProxy.set(self)
+
+    }
+
+    func updateActivity() {
+        activityInfo.title = activityTitle.textField.text!
+        activityInfo.memo = memo.textView.text
+        ActivityService.instance.save(activityInfo, onCompletion: {
+            info in
+            if self.onEditComplete != nil {
+                self.onEditComplete!(info)
+            }
+            self.navigationController?.popViewController(animated: true)
+        },
+                                      onError: { error in
+                                          print(error)
+                                      })
+
+    }
+
+    var onEditComplete: ((_ info: ActivityInfo) -> Void)?
+}
---
+++ mitty/ProfileViewController~.swift
@@ -0,0 +1,608 @@
+//
+//  ProfileViewController.swift
+//  mitty
+//
+//  Created by gridscale on 2017/08/24.
+//  Copyright © 2017年 GridScale Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+import AlamofireImage
+import SwiftyJSON
+
+// todo 改造中
+class ProfileViewController: MittyViewController {
+
+    var mittyId: Int!
+
+    var userInfo = UserInfo()
+    var profile = Profile()
+    var socialIdList = [SocialId]()
+    var nameCardBox = [ContacteeNamecard]()
+    var content: Content? = Content()
+
+    var form = MQForm.newAutoLayout()
+
+    let picture: Control = MQForm.button(name: "m2", title: "")
+
+    let idLabel = MQForm.label(name: "mitty-id", title: "")
+    let nameLabel = MQForm.label(name: "name", title: "秘密")
+    let genderLabel = MQForm.hilight(label: "秘密", named: "gender")
+    let ageGroupLabel = MQForm.hilight(label: "秘密", named: "age-group")
+    let speech = MQForm.textView(name: "speech")
+
+    let constellationLabel = MQForm.hilight(label: "秘密", named: "constellation")
+    let homeIslandLabel = MQForm.hilight(label: "秘密", named: "home-island")
+
+    let birthIslandLabel = MQForm.hilight(label: "秘密", named: "birth-island")
+
+    let appearanceLabel = MQForm.hilight(label: "秘密", named: "appearence")
+
+    let occupationLabel1 = MQForm.hilight(label: "未設定", named: "occupation1")
+    let occupationLabel2 = MQForm.hilight(label: "未設定", named: "occupation2")
+    let occupationLabel3 = MQForm.hilight(label: "未設定", named: "occupation3")
+
+    let hobbyLabel1 = MQForm.hilight(label: "未設定", named: "hobby1")
+    let hobbyLabel2 = MQForm.hilight(label: "未設定", named: "hobby2")
+    let hobbyLabel3 = MQForm.hilight(label: "未設定", named: "hobby3")
+    let hobbyLabel4 = MQForm.hilight(label: "未設定", named: "hobby4")
+    let hobbyLabel5 = MQForm.hilight(label: "未設定", named: "hobby5")
+    let blank = MQForm.label(name: "blank", title: "")
+
+    let nameCardSection = Section(name: "namecard-section")
+
+    let socialIdSection = Section(name: "social-id-section")
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        super.autoCloseKeyboard()
+
+        self.view.backgroundColor = UIColor.white
+
+        buildform()
+        self.view.addSubview(form)
+
+        UserService.instance.getUserInfo(id: String(mittyId), callback: {
+            uinfo, exists in
+            if exists {
+                self.userInfo = uinfo!
+                ProfileService.instance.profile(of: self.mittyId, onComplete: {
+                    p in
+                    self.profile = p
+
+                    SocialContactService.instance.contactedNamecards(of: p.mittyId, onComplete: {
+                        cardlist in
+                        self.nameCardBox = cardlist
+                        p.addObserver(handler: { o in
+                            self.setUserProfile()
+                        })
+                        p.notify()
+                        self.addCardList()
+                    }, onError: { error in
+                        self.showError(error)
+                    })
+                }, onError: { error in
+                    self.showError(error)
+                })
+            } else {
+                self.showError("User Not Exists")
+            }
+        })
+
+
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        view.setNeedsUpdateConstraints()
+    }
+
+    override func updateViewConstraints() {
+
+        super.updateViewConstraints()
+
+        form.autoPin(toTopLayoutGuideOf: self, withInset: 10)
+        form.autoPinEdge(toSuperviewEdge: .left)
+        form.autoPinEdge(toSuperviewEdge: .right)
+        form.autoPinEdge(toSuperviewEdge: .bottom)
+
+        form.configLayout()
+
+    }
+
+    func buildform () {
+
+        form.backgroundColor = UIColor(patternImage: UIImage(named: "beauty2.jpeg")!)
+        let anchor = MQForm.label(name: "dummy", title: "").layout {
+            a in
+            a.height(0).leftMost().rightMost()
+        }
+
+        form +++ anchor
+        
+        
+        // スクロールViewを作る
+        let scroll = UIScrollView.newAutoLayout()
+//        scroll.contentSize = CGSize(width: UIScreen.main.bounds.size.width, height: 900)
+        scroll.isScrollEnabled = true
+        scroll.flashScrollIndicators()
+        scroll.canCancelContentTouches = false
+        self.automaticallyAdjustsScrollViewInsets = false
+        //        scroll.layer.borderWidth = 1
+        //        scroll.layer.borderColor = UIColor.blue.cgColor
+
+        let scrollContainer = Container(name: "Detail-form", view: scroll).layout() { (container) in
+            container.fillParent()
+        }
+
+        form +++ scrollContainer
+
+        let detailForm = Section(name: "Content-Form", view: UIView.newAutoLayout())
+        detailForm.lineSpace = 3
+
+        scrollContainer +++ detailForm
+
+        loadPicture()
+
+
+        // bild Identity
+        buildIdentity(detailForm)
+
+        // build Speech
+        buildSpeech(detailForm)
+        
+        
+        // build Social Links
+        buildSocialLinks(detailForm)
+        
+        // build name cards
+        buildNameCards(detailForm)
+
+        // build biography
+        buildBiography(detailForm)
+
+        let bottom = Row.LeftAligned()
+        detailForm <<< bottom
+
+        detailForm.layout {
+            f in
+            f.fillVertical().width(UIScreen.main.bounds.width).bottomAlign(with: bottom)
+            f.view.autoSetDimension(.height, toSize: UIScreen.main.bounds.height + 10, relation: .greaterThanOrEqual)
+            f.view.backgroundColor = UIColor.white
+        }
+        
+        // build Online status/teleport at the bottom of form
+        buildPresence(form)
+    }
+
+    func loadPicture() {
+        picture.height(30).width(30)
+        picture.button.contentMode = .scaleAspectFit
+        picture.button.backgroundColor = .clear
+        if userInfo.icon != "" {
+            picture.button.setMittyImage(url: userInfo.icon)
+        } else {
+            picture.button.setImage(UIImage(named: "pengin2"), for: .normal)
+        }
+
+    }
+
+
+    func buildIdentity(_ section: Section) {
+
+        var row = seperator(section: section, caption: "基本情報")
+
+        section <<< row
+
+        row = Row.LeftAligned()
+        row.layout {
+            r in
+            r.height(120).fillHolizon()
+        }
+
+        let col1 = Col.UpDownAligned()
+        col1.layout {
+            c in
+            c.leftMost().height(120).width(120).verticalCenter()
+        }
+        col1 +++ picture
+        picture.layout {
+            p in
+            p.fillParent(withInset: 30)
+        }
+        row +++ col1
+
+        let col2 = Col.UpDownAligned()
+        col2.layout {
+            c in
+            c.fillVertical().rightMost()
+        }
+
+        let idRow = Row.LeftAligned()
+        idRow.layout {
+            r in
+            r.fillHolizon().height(25)
+        }
+
+        idRow +++ MQForm.label(name: "id", title: "ID:").layout {
+            l in
+            l.verticalCenter()
+        }
+        idRow +++ idLabel.layout {
+            l in
+            l.label.textAlignment = .center
+            l.rightMost(withInset: 10).height(25).verticalCenter()
+        }
+
+
+        col2 +++ idRow
+
+        let nameRow = Row.LeftAligned()
+        nameRow.layout {
+            r in
+            r.fillHolizon().height(25)
+        }
+
+        nameRow +++ MQForm.label(name: "name", title: "名前").layout {
+            l in
+            l.verticalCenter()
+        }
+
+        nameRow +++ nameLabel.layout {
+            l in
+            l.label.textAlignment = .center
+            l.rightMost(withInset: 10).height(25).width(120).verticalCenter()
+        }
+
+
+        col2 +++ nameRow
+
+        let genderRow = Row.LeftAligned()
+        genderRow.layout {
+            r in
+            r.fillHolizon().height(30)
+        }
+
+        genderRow +++ MQForm.label(name: "gender", title: "性別").layout {
+            l in
+            l.verticalCenter()
+        }
+
+        genderRow +++ genderLabel.layout {
+            l in
+            l.label.textAlignment = .center
+            l.rightMost(withInset: 10).height(25).width(120).verticalCenter()
+        }
+
+
+        col2 +++ genderRow
+
+
+        let ageGroupRow = Row.LeftAligned()
+        ageGroupRow.layout {
+            r in
+            r.fillHolizon().height(30)
+        }
+
+        ageGroupRow +++ MQForm.label(name: "age-group", title: "年齢層").layout {
+            l in
+            l.verticalCenter()
+        }
+
+        ageGroupRow +++ ageGroupLabel.layout {
+            l in
+            l.label.textAlignment = .center
+            l.rightMost(withInset: 10).height(25).width(120).verticalCenter()
+        }
+
+        col2 +++ ageGroupRow
+
+
+        row +++ col2
+
+        section <<< row
+
+
+    }
+
+    func buildSpeech(_ section: Section) {
+
+        let row = seperator(section: section, caption: "One word of speech")
+
+        section <<< row
+
+        let row1 = Row.LeftAligned()
+        row1.layout {
+            r in
+            r.height(50).fillHolizon()
+        }
+        row1.spacing = 10
+        speech.textView.text = profile.oneWordSpeech
+        row1 +++ speech.layout {
+            s in
+            s.fillParent(withInset: 5)
+        }
+
+        section <<< row1
+
+    }
+
+    func buildBiography(_ section: Section) {
+
+        var row = seperator(section: section, caption: "Biography")
+
+
+        section <<< row
+
+        row = Row.LeftAligned().layout {
+            r in
+            r.fillHolizon().height(40)
+        }
+
+        row +++ MQForm.label(name: "constellationLabel", title: "星座").height(30).width(70).layout {
+            l in
+            l.verticalCenter().leftMargin(15)
+        }
+        row +++ constellationLabel.layout {
+            l in
+            l.height(30).width(120).verticalCenter().leftMargin(15)
+            l.margin.left = 80
+        }
+
+        section <<< row
+
+        row = Row.LeftAligned().layout {
+            r in
+            r.fillHolizon().height(40)
+        }
+
+        row +++ MQForm.label(name: "appearanceLabel", title: "外見").height(35).width(70).layout {
+            l in
+            l.verticalCenter().leftMargin(15)
+        }
+
+        row +++ appearanceLabel.layout {
+            l in
+            l.height(30).width(120).verticalCenter()
+            l.margin.left = 80
+        }
+
+        section <<< row
+
+        row = seperator(section: section, caption: "職業").layout {
+            l in
+            l.verticalCenter()
+        }
+
+
+        section <<< row
+
+        row = Row.Intervaled().layout {
+            r in
+            r.fillHolizon().height(42)
+        }
+        row.spacing = 5
+        section <<< row
+
+        row +++ occupationLabel1
+        row +++ occupationLabel2
+        row +++ occupationLabel3
+
+        row = seperator(section: section, caption: "趣味")
+
+        section <<< row
+
+        // Hobby Line1
+        row = Row.Intervaled().layout {
+            r in
+            r.fillHolizon().height(42)
+        }
+        row.spacing = 5
+        section <<< row
+
+        row +++ hobbyLabel1
+        row +++ hobbyLabel2
+        row +++ hobbyLabel3
+
+        // Hobby Line2
+        row = Row.Intervaled().layout {
+            r in
+            r.fillHolizon().height(42)
+        }
+        row.spacing = 5
+        section <<< row
+
+        row +++ hobbyLabel4
+        row +++ hobbyLabel5
+        row +++ blank
+    }
+
+    /// <#Description#>
+    ///
+    /// - Parameter section: <#section description#>
+    func buildSocialLinks(_ section: Section) {
+        let row = seperator(section: section, caption: "ソーシャルID")
+
+        section <<< row
+
+        let row1 = Row.LeftAligned()
+        row1.layout {
+            r in
+            r.height(30).fillHolizon()
+        }
+        row1 +++ MQForm.label(name: "Line", title: "LINE").layout {
+            s in
+            s.verticalCenter().leftMargin(15)
+        }
+
+        section <<< row1
+
+    }
+
+    func buildNameCards(_ section: Section) {
+        let row = seperator(section: section, caption: "名刺")
+
+        section <<< row
+
+        let row1 = Row.LeftAligned()
+        row1.layout {
+            r in
+            r.bottomAlign(with: self.nameCardSection).fillHolizon()
+        }
+
+        row1 +++ nameCardSection.layout {
+            s in
+            s.upper()
+        }
+
+        section <<< row1
+
+    }
+    
+    func buildPresence(_ form: MQForm) {
+        
+        let row = Row.Intervaled()
+        row.layout {
+            r in
+            r.fillHolizon().height(40).down()
+            r.view.backgroundColor = UIColor.white
+        }
+        
+        let row1 = Row.LeftAligned().layout{
+            r in
+            r.fillVertical()
+        }
+        row +++ row1
+
+        row1 +++ MQForm.tapableImg(name: "online", url: "online.jpeg").layout {
+            im in
+            im.height(35).width(35).verticalCenter().leftMargin(25)
+        }
+
+        row1 +++ MQForm.label(name: "online-offline", title: "Online").layout {
+            l in
+            l.verticalCenter().leftMargin(10)
+            l.label.textColor = UIColor.gray
+        }
+        
+        let row2 = Row.LeftAligned().layout {
+            r in
+            r.fillVertical()
+        }
+        row +++ row2
+        row2 +++ MQForm.tapableImg(name: "teleport", url: "teleport").layout {
+            im in
+            im.height(35).width(35).verticalCenter().leftMargin(25)
+        }
+        
+        row2 +++ MQForm.button(name: "online-Teleport", title: "Teleport").layout {
+            l in
+            l.view.backgroundColor = MittyColor.white
+            l.button.setTitleColor(MittyColor.healthyGreen, for: .normal)
+            l.verticalCenter().leftMargin(5)
+        }
+        
+        form +++ row
+        
+    }
+
+
+    /// <#Description#>
+    func setUserProfile() {
+
+        // Basic Info
+        idLabel.label.text = String(format: "%010d", userInfo.id)
+        nameLabel.label.text = userInfo.userName
+        genderLabel.label.text = profile.gender == "" ? "秘密" : gendre(of: profile.gender)?.value
+        ageGroupLabel.label.text = profile.ageGroup == "" ? "秘密" : agegroup(of: profile.ageGroup)?.value
+        if userInfo.icon != "" {
+            picture.button.setMittyImage(url: userInfo.icon)
+        }
+
+        speech.textView.text = profile.oneWordSpeech
+
+        // Biography
+        constellationLabel.label.text = profile.constellation == "" ? "秘密" : CONSTELLATION(of: profile.constellation)?.value
+
+        appearanceLabel.label.text = profile.appearanceTag == "" ? "秘密" : appearance(of: profile.appearanceTag)?.value
+
+        occupationLabel1.label.text = profile.occupationTag1 == "" ? LS(key: "not-set") : occupation(of: profile.occupationTag1)?.value
+        occupationLabel2.label.text = profile.occupationTag2 == "" ? LS(key: "not-set") : occupation(of: profile.occupationTag2)?.value
+        occupationLabel3.label.text = profile.occupationTag3 == "" ? LS(key: "not-set") : occupation(of: profile.occupationTag3)?.value
+
+        hobbyLabel1.label.text = profile.hobbyTag1 == "" ? LS(key: "not-set") : hobby(of: profile.hobbyTag1)?.value
+        hobbyLabel2.label.text = profile.hobbyTag2 == "" ? LS(key: "not-set") : hobby(of: profile.hobbyTag2)?.value
+        hobbyLabel3.label.text = profile.hobbyTag3 == "" ? LS(key: "not-set") : hobby(of: profile.hobbyTag3)?.value
+        hobbyLabel4.label.text = profile.hobbyTag4 == "" ? LS(key: "not-set") : hobby(of: profile.hobbyTag4)?.value
+        hobbyLabel5.label.text = profile.hobbyTag5 == "" ? LS(key: "not-set") : hobby(of: profile.hobbyTag5)?.value
+
+
+    }
+
+    func addCardList() {
+        var last: Row? = nil
+        for card in nameCardBox {
+            let row = Row.LeftAligned().layout {
+                r in
+                r.height(30).fillHolizon()
+            }
+
+            row +++ MQForm.img(name: "businessLogo", url: "").layout {
+                i in
+                i.imageView.setMittyImage(url: card.businessLogoUrl)
+                i.height(25).width(25).verticalCenter().leftMargin(15)
+            }
+            row +++ MQForm.label(name: "namecard", title: card.businessName).layout {
+                l in
+                l.height(30).rightMost(withInset: 30).leftMargin(5).verticalCenter()
+                l.label.textColor = UIColor.orange
+            }
+            
+            if card.contactID == 0 {
+                row +++ MQForm.tapableImg(name: "exchange", url: "excard").layout {
+                    l in
+                    l.rightMost(withInset: 8).width(22).height(20).verticalCenter()
+                }.bindEvent(.touchUpInside) {
+                        b in
+                        let vc = NamecardExchangeViewController()
+                        vc.contacteeCard = card
+                        vc.contacteeUser = self.userInfo
+                        self.navigationController?.pushViewController(vc, animated: true)
+                }
+            } else {
+                row +++ MQForm.label(name: "view", title: ">").layout {
+                    l in
+                    l.rightMost().width(40).verticalCenter()
+                }.bindEvent(.touchUpInside) { l in
+                    l.isUserInteractionEnabled = false
+                    NameCardService.instance.getNamecard(of: card.namecardID, onComplete: { nc in
+                        let vc = NameCardViewController()
+                        vc.nameCard = nc
+                        self.navigationController?.pushViewController(vc, animated: true)
+                    }, onError: {
+                        error in
+                        self.showError(error)
+                    })
+                }
+            }
+
+            last = row
+            nameCardSection <<< row
+        }
+
+
+        if (last != nil) {
+            nameCardSection.layout {
+                s in
+                s.bottomAlign(with: last!).fillHolizon()
+            }
+            nameCardSection.configLayout()
+            self.view.setNeedsUpdateConstraints()
+        }
+    }
+
+}
